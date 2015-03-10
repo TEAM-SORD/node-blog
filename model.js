462822cc5a78c25ec6c11356c49afddd40e3eb1f
sarah <sarahabimay@gmail.com>
2015-03-10 15:09:55 +0000

    create and home handler and db stuff

--- model.js
+++ model.js
@@ -1,1 +1,82 @@
-// Nothing here yet
+// Require mongoose and other node modules \\
+var mongoose = require("mongoose");
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogpostdb");
+
+// Get notification for connection success or failure \\
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function (callback) {
+	console.log("connection made");
+});
+
+// Define database schema which determines which properties we want to store \\
+var blogSchema = mongoose.Schema({
+	author : String,
+	title  : String,
+	text   : String,
+	date   : Object,
+	image  : String
+});
+
+// Adding method to the schema. Have to be defined before schema is compiled \\
+blogSchema.methods.announce = function() {
+	var author = this.author ? "Another blog post by " + this.author : "Anonymous blog post";
+	console.log(author);
+};
+
+// Compile schema into a model, which defines the database collection \\
+// First argument is collection name, second argument is schema name  \\
+var blogPostModel = mongoose.model("blogpost", blogSchema);
+
+
+
+
+module.exports = {
+
+	getPosts : function( searchCriteria, doSomethingWithResults ) {
+	    return blogPostModel.find();/*function(err, blogPosts) {
+			console.log( 'In blogPostModel : ' + blogPosts);
+			if( err ) {
+				console.log( 'Error: ' + err );
+			}
+			doSomethingWithResults( blogPosts );
+		});*/
+
+	},
+	addPost  : function( newPost, successCB ) {
+		var post = new blogPostModel(newPost);
+		console.log( 'Post model: ' + post );
+		post.save(function (err) {
+		  if (err) {
+		  	console.log( 'Error: ' + err );
+		  } 
+		  console.log( 'Saved to the collection!');
+		  successCB( err );
+		});
+	},
+	updatePost : function( post ) {
+
+	},
+	deletePost : function( post ) {
+
+	}
+};
+
+// Example blog post \\
+// var testPost = new blogPostModel({ author : "bob smith",
+// 							   title : "read these words", 
+// 							   	text : "this is some informatioon about an interesting topic of my choice",
+// 							    date : "new data object",
+// 							   image : "img src ='www.google.com/images/pineapple"
+// 							});
+
+// // Saves submitted blog post to database and displays a message confirming \\
+// testPost.save(function(err, testPost){
+// 	if (err) return console.error(err);
+// 	testPost.announce();
+// });
+
--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 var http = require("http");
 var handler = require("./handler");
 var port = 4000;
-var app = http.createServer(handler);
 
-app.listen(port);
+
+//*** List of Routes and Associated Handler Functions ***//
+var routes = {};
+routes["/"] = handler.home; 
+routes["/blogpage"] = handler.blogpage;// localhost:4000/open_post?id=blogid
+routes["/editpage"] = handler.editpage;// localhost:4000/open_edit?id=blogid
+routes["/read"] = handler.read;
+routes["/create"] = handler.create;
+routes["/update"] = handler.update;
+routes["/delete"] = handler.delete;
+
+
+//*** Invokes the right handler or throws error ***//
+var router = function(req, res){
+    var url = req.url;
+    console.log("request received for ", url);
+
+
+    if (typeof routes[url] === 'function'){
+        routes[url](req, res); 
+    } 
+    else {
+        console.log('Error, route for ', url, 'does not exist');
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end(" ERROR!!");
+    }
+};
+
+
+http.createServer(router).listen(port);
 
 console.log('Server running on port', port);
