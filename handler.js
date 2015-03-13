2375b551c025843546c10a0aad5dbf41ed0785fd
sarah <sarahabimay@gmail.com>
2015-03-13 12:26:05 +0000

    delete unused functions and commented out code

--- handler.js
+++ handler.js
@@ -9,24 +9,6 @@ var querystring = require('querystring'),
 	utils = require('util');
 
 var allPosts = [];
-// var handleResponse = function( res, responseData ){
-// 	res.writeHead(200, {"Content-Type": responseData.contentType});
-//     //res.write("Welcome");
-//     res.end(responseData.data);
-// };
-
-// function handleHTML( requestType, res, handleResponse, pushData ) {
-// 	if( requestType === 'GET'){
-// 		return {
-// 				  contentType: 'text/plain',
-// 				  data       : '<html><body> <h3> HELLO WORLD</h3></body></html>'
-// 			   };
-//    	}
-//    	else if( requestType === 'POST' ){
-//    		// not sure how we post to a server side rendered system, probably just json!
-// 	}
-// }
-
 
 function getSearchData( req, processData ) {
 	var fullBody = '';
@@ -66,59 +48,6 @@ function renderPage( whichPage, data ){
 	if( fn ) {
 		console.log( 'fn exists: data.posts: '+ data.posts );
 		return fn ( data);
-	}
-}
-
-function renderPageById( req, pageType, sendResponse ){
-	var requestType = req.method;
-	console.log( 'request type in renderPageById: ' + requestType );
-	if( requestType === 'GET') {
-		console.log( 'In GET' );
-		var reqURL = url.parse( req.url, true );
-		var blog_id = reqURL.query.id;
-		var query = db.getPosts( blog_id );
-		query.exec(function (err, posts) {
-			console.log( 'In GET query.exec : ' + posts );
-			sendResponse( renderPage( pageType, posts ) );		
-		});
-	}
-	else if( requestType === 'PUT '){
-		console.log( 'In PUT' );
-		req.on('data', function(chunk) {
-      		// append the current chunk of data to the fullBody variable
-      		fullBody += chunk.toString();
-    	});
-    
-    	req.on('end', function() {
-			console.log( 'fullBody: ' + fullBody );
-			var decodedBody = querystring.parse(fullBody);
-			console.log( 'DecodedBody: ' + decodedBody );
-			db.updatePost(decodedBody, function(err, numAffected) {
-  				// numAffected is the number of updated documents
-				console.log( 'Error posting to db: ' + err );
-				console.log( 'Number of posts updated (should be one): ' + numAffected );
-				sendResponse( renderPage( pageType, posts ) );
-			});
-		});
-	}
-	else if( requestType === 'POST' ) {
-		console.log( 'In POST' );
-		//getSearchData( req, function() {
-		var fullBody = '';
-        
-    	req.on('data', function(chunk) {
-      		// append the current chunk of data to the fullBody variable
-      		fullBody += chunk.toString();
-    	});
-    
-    	req.on('end', function() {
-			console.log( 'fullBody: ' + fullBody );
-			var decodedBody = querystring.parse(fullBody);
-			console.log( 'DecodedBody: ' + decodedBody );
-			db.addPost(decodedBody, function(err){
-				console.log( 'Error posting to db: ' + err );
-			});
-		});
 	}
 }
 
